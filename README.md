40432f1563c83479c6b14e85ed88a48109d2c942
Lumina Cash <lumina.cash@gmail.com>
2022-01-29 16:06:55 -0800

    First DEMO version deplyed to Mumbai network

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lumina Cash Mining Page
+
+* [https://luminacash.github.io](https://luminacash.github.io)
---
+++ precache-manifest.1bebfeaf4303802daa87b49f1737771a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea618c6b2e88287022e66396c5fe752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2e6b0eeb33555fe948",
+    "url": "/static/css/main.35e940e2.chunk.css"
+  },
+  {
+    "revision": "48edbb3a2cd9dd4890af",
+    "url": "/static/js/2.663a89f1.chunk.js"
+  },
+  {
+    "revision": "b0b3f6b24b6dd624b18d1a89c6f3b57e",
+    "url": "/static/js/2.663a89f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2e6b0eeb33555fe948",
+    "url": "/static/js/main.9ed500ba.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
